767c08f736dd9671cbb6467c575910a3d87ca318
madgab <angaag@gmail.com>
2017-07-09 16:59:16 +0200

    chg: back to pythonanywhere.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn rimini_blog.wsgi
--- rimini_blog/settings.py
+++ rimini_blog/settings.py
@@ -123,24 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'gabindev.pythonanywhere.com']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-import dj_database_url
-
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
